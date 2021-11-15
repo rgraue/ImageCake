772b81e319b25677c7cd3e59534d0014f82f3454
Ryan Graue <grauer@spu.edu>
2021-11-14 23:26:16 -0800

    idk again

--- modules/layer.js
+++ modules/layer.js
@@ -12,17 +12,7 @@ class Layer {
             top = this._png;
             bottom = rhs._png;
         }
-        for (let y = 0; y < top.height; y++) {
-            for (let x = 0; x < top.width; x++){
-                let idx = (x + y) << 2 ;
-        
-                bottom.data[idx] = top.data[idx];
-                bottom.data[idx + 1] = top.data[idx + 1];
-                bottom.data[idx + 2] = top.data[idx + 2];
-
-                //bottom.data[idx + 3] = top.data[idx + 3] >> 1;
-            }
-        }
+        // overlap here
     }
 
     // Write () {
