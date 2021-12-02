e2bb3f3be8625be925307b2828d3682b2133e8f8
Ryan Graue <grauer@spu.edu>
2021-12-01 23:40:44 -0800

    img overlay with x,y offset

--- modules/layer.js
+++ modules/layer.js
@@ -1,3 +1,4 @@
+// Layer class to represent a layer of a total image
 class Layer {
     constructor(png, level  = 0){
         this._png = png;
@@ -5,19 +6,45 @@ class Layer {
 
     }
 
+    // meshes two images togther. The lower level is set as the base.
+    // The top is placed 'on top' of the base
     Mesh (rhs) {
+        //sets the base and top
         let top = rhs._png;
         let bottom = this._png;
+
+        //flips the top and bottom depending on level
         if (this._level > rhs._level){
             top = this._png;
             bottom = rhs._png;
         }
-        // overlap here
-    }
 
-    // Write () {
-    //     return this._png.sync.;
-    // }
+        // offset to move top image over and down. starts at top left (0,0)
+        let xOffest = 100;
+        let yOffset = 100;
+        
+        // iterates through the top image buffer.
+        for (let y = 0; y<top.height;y++){
+            for (let x = 0; x<top.width;x++){
+                let indexB = ((bottom.width * (y+ yOffset)  + x) + xOffest)<< 2; // Bottom index
+                let indexT = (top.width * y + x) << 2; // Top index
+
+                // determines whether part of images is transparent
+                if ((top.data[indexT] !== 0xee && top.data[indexT] !== 0xee &&
+                        top.data[indexT + 2] !== 0xee) 
+                        && 
+                    (top.data[indexT] !== 0xff && top.data[indexT] !== 0xff &&
+                        top.data[indexT + 2] !== 0xff)){
+
+                    // if not transaprent replace base pixel data with top pixel data (rgba)
+                    bottom.data[indexB] = top.data[indexT];
+                    bottom.data[indexB + 1] = top.data[indexT + 1];
+                    bottom.data[indexB + 2] = top.data[indexT + 2];
+                    bottom.data[indexB + 3] = top.data[indexT + 3];
+                }
+            }
+        }
+    }
 
      toString () {
         return "" + _png.data;
