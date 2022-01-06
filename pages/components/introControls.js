21cf997cc34d4ee5d8b1cc4a057d52e177c5459b
Ryan Graue <grauer@spu.edu>
2022-01-06 13:26:29 -0800

    conditional render origin enter

--- pages/components/introControls.js
+++ pages/components/introControls.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import Button from '../elements/button';
 import Search from '../elements/search';
 
 class IntroControls extends React.Component{
+    constructor(){
+        super();
+        this.handleEnterOrgin = this.handleEnterOrgin.bind(this);
+        this.createCake = this.createCake.bind(this);
+        this.state = {showSubmit : false};
+    }
+
+    createCake(){
+        console.log("hit")
+    }
+
+    handleEnterOrgin () {
+        this.setState({showSubmit : true});
+    }
+
     render () {
+        let submit;
+        if (this.state.showSubmit) {
+            submit =    <div className='row'>
+                            <Search/>
+                        </div>
+        }
         return (
             <div>
                 <div className='row'>
                     <div className='col'>
-                        <Button text={'Create new Cake'}/>
+                        <button type='button' className='btn btn-primary btn-lg' 
+                            onClick={this.createCake}>Bake New Cake</button>
                     </div>
                     <div className='col'>
-                        <Button text={'Already have Cake Origin'} />
+                        <button type='button' className='btn btn-primary btn-lg' 
+                            onClick={this.handleEnterOrgin}>Already Have Cake Origin</button>
                     </div>
                 </div>
-                <div className='row'>
-                    <Search/>
-                </div>
+                {submit}
             </div>
         )
     }
