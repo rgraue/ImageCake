65907253d981db5439c2bdd604aaf895d640050f
rgraue <rgraue@outlook.com>
2022-01-05 17:53:35 -0800

    react comps and bootstrap css

---
+++ pages/components/cakeDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CakeDisplay extends React.Component {
+    render () {
+        return (
+            <h2>Second Component</h2>
+        )
+    }
+}
+
+export default CakeDisplay;
